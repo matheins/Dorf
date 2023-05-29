a6ac829ae8b9f1df294fd52ac6f10361b77191be
matheins <mail@mheins.de>
2023-05-29 19:05:00 +0200

    feat: add type safe env vars

---
+++ src/env.mjs
@@ -0,0 +1,31 @@
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
+
+export const env = createEnv({
+  server: {
+    // This is optional because it's only used in development.
+    // See https://next-auth.js.org/deployment.
+    NEXTAUTH_URL: z.string().url().optional(),
+    // NEXTAUTH_SECRET: z.string().min(1),
+    // GITHUB_CLIENT_ID: z.string().min(1),
+    // GITHUB_CLIENT_SECRET: z.string().min(1),
+    // GITHUB_ACCESS_TOKEN: z.string().min(1),
+    // DATABASE_URL: z.string().min(1),
+    // SMTP_FROM: z.string().min(1),
+    // POSTMARK_API_TOKEN: z.string().min(1),
+    // POSTMARK_SIGN_IN_TEMPLATE: z.string().min(1),
+    // POSTMARK_ACTIVATION_TEMPLATE: z.string().min(1),
+  },
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().min(1),
+  },
+  runtimeEnv: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    // DATABASE_URL: process.env.DATABASE_URL,
+    // SMTP_FROM: process.env.SMTP_FROM,
+    // POSTMARK_API_TOKEN: process.env.POSTMARK_API_TOKEN,
+    // POSTMARK_SIGN_IN_TEMPLATE: process.env.POSTMARK_SIGN_IN_TEMPLATE,
+    // POSTMARK_ACTIVATION_TEMPLATE: process.env.POSTMARK_ACTIVATION_TEMPLATE,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
+})
