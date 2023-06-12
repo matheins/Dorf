c698067e512e64e9ec9163375fc8095d3dcb7d90
matheins <mail@mheins.de>
2023-06-13 00:37:07 +0200

    feat: basic next-auth setup

---
+++ src/lib/auth.ts
@@ -0,0 +1,110 @@
+import { eq } from "drizzle-orm"
+import { NextAuthOptions } from "next-auth"
+import EmailProvider from "next-auth/providers/email"
+import GitHubProvider from "next-auth/providers/github"
+import { Client } from "postmark"
+
+import { env } from "@/env.mjs"
+import { siteConfig } from "@/config/site"
+import { db } from "@/lib/db"
+
+import { PlanetScaleAdapter } from "./db/lib/drizzle-adapter"
+import { users } from "./db/schema"
+
+const postmarkClient = new Client(env.POSTMARK_API_TOKEN)
+
+export const authOptions: NextAuthOptions = {
+  adapter: PlanetScaleAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    GitHubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      from: env.SMTP_FROM,
+      sendVerificationRequest: async ({ identifier, url, provider }) => {
+        const user = await db.query.users.findFirst({
+          where: eq(users.email, identifier),
+          columns: {
+            emailVerified: true,
+          },
+        })
+
+        const templateId = user?.emailVerified
+          ? env.POSTMARK_SIGN_IN_TEMPLATE
+          : env.POSTMARK_ACTIVATION_TEMPLATE
+        if (!templateId) {
+          throw new Error("Missing template id")
+        }
+
+        const result = await postmarkClient.sendEmailWithTemplate({
+          TemplateId: parseInt(templateId),
+          To: identifier,
+          From: provider.from as string,
+          TemplateModel: {
+            action_url: url,
+            product_name: siteConfig.name,
+          },
+          Headers: [
+            {
+              // Set this to prevent Gmail from threading emails.
+              // See https://stackoverflow.com/questions/23434110/force-emails-not-to-be-grouped-into-conversations/25435722.
+              Name: "X-Entity-Ref-ID",
+              Value: new Date().getTime() + "",
+            },
+          ],
+        })
+
+        if (result.ErrorCode) {
+          throw new Error(result.Message)
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+      }
+
+      return session
+    },
+    async jwt({ token, user }) {
+      //   const dbUser = await db.user.findFirst({
+      //     where: {
+      //       email: token.email,
+      //     },
+      //   })
+
+      let dbUser
+      if (token.email) {
+        dbUser = await db.query.users.findFirst({
+          where: eq(users.email, token.email),
+        })
+      }
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id
+        }
+        return token
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
+    },
+  },
+}
