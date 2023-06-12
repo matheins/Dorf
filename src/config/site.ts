90ac06446acfadd1210de82ee3df41a0e32dc3b1
matheins <mail@mheins.de>
2023-06-12 13:00:46 +0200

    chore: update links

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,10 +3,10 @@ import { SiteConfig } from "@/types"
 export const siteConfig: SiteConfig = {
   name: "Dorf forms",
   description: "A free and open source form builder.",
-  url: "https://dorf.build",
-  ogImage: "https://dorf.build/og.jpg",
+  url: "https://dorf.vercel.app",
+  ogImage: "https://dorf.vercel.app/og.jpg",
   links: {
     twitter: "https://twitter.com/matheins",
-    github: "https://github.com/matheins/dorf-forms",
+    github: "https://github.com/matheins/dorf",
   },
 }
