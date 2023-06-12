cb9494a368b41c94d10b94c7e3be955c0734eec1
matheins <mail@mheins.de>
2023-06-13 00:41:12 +0200

    feat: add user nav

--- src/components/user-account-nav.tsx
+++ src/components/user-account-nav.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import Link from "next/link"
+import { User } from "next-auth"
+import { signOut } from "next-auth/react"
 
 // import { User } from "next-auth"
 // import { signOut } from "next-auth/react"
@@ -15,41 +17,41 @@ import {
 import { UserAvatar } from "@/components/user-avatar"
 
 interface UserAccountNavProps extends React.HTMLAttributes<HTMLDivElement> {
-  //   user: Pick<User, "name" | "image" | "email">
+  user: Pick<User, "name" | "image" | "email">
 }
 
-export function UserAccountNav({}: UserAccountNavProps) {
+export function UserAccountNav({ user }: UserAccountNavProps) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        {/* <UserAvatar
+        <UserAvatar
           user={{ name: user.name || null, image: user.image || null }}
           className="h-8 w-8"
-        /> */}
+        />
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        {/* <div className="flex items-center justify-start gap-2 p-2">
+        <div className="flex items-center justify-start gap-2 p-2">
           <div className="flex flex-col space-y-1 leading-none">
             {user.name && <p className="font-medium">{user.name}</p>}
             {user.email && (
-              <p className="w-[200px] truncate text-sm text-muted-foreground">
+              <p className="text-muted-foreground w-[200px] truncate text-sm">
                 {user.email}
               </p>
             )}
           </div>
-        </div> */}
+        </div>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
           <Link href="/dashboard">Dashboard</Link>
         </DropdownMenuItem>
-        <DropdownMenuItem asChild>
+        {/* <DropdownMenuItem asChild>
           <Link href="/dashboard/billing">Billing</Link>
-        </DropdownMenuItem>
+        </DropdownMenuItem> */}
         <DropdownMenuItem asChild>
           <Link href="/dashboard/settings">Settings</Link>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
-        {/* <DropdownMenuItem
+        <DropdownMenuItem
           className="cursor-pointer"
           onSelect={(event) => {
             event.preventDefault()
@@ -59,7 +61,7 @@ export function UserAccountNav({}: UserAccountNavProps) {
           }}
         >
           Sign out
-        </DropdownMenuItem> */}
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
