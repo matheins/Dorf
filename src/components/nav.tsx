01a286a090a22307842a298df0674ac72520de51
matheins <mail@mheins.de>
2023-06-13 01:39:19 +0200

    fix: use pathname for dynamic route

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -24,7 +24,10 @@ export function DashboardNav({ items }: DashboardNavProps) {
         const Icon = Icons[item.icon || "arrowRight"]
         return (
           item.href && (
-            <Link key={index} href={item.disabled ? "/" : item.href}>
+            <Link
+              key={index}
+              href={{ pathname: item.disabled ? "/" : item.href }}
+            >
               <span
                 className={cn(
                   "hover:bg-accent hover:text-accent-foreground group flex items-center rounded-md px-3 py-2 text-sm font-medium",
