77ad43f74ad374454f732127d1bcf9276a1e12db
matheins <mail@mheins.de>
2023-06-14 13:52:18 +0200

    feat: generate dynamic og for forms

--- src/app/(form)/f/[id]/page.tsx
+++ src/app/(form)/f/[id]/page.tsx
@@ -1,11 +1,20 @@
+import { Metadata } from "next"
 import { notFound } from "next/navigation"
 import { eq } from "drizzle-orm"
 
+import { env } from "@/env.mjs"
 import { db } from "@/lib/db"
 import { forms } from "@/lib/db/schema"
+import { absoluteUrl } from "@/lib/utils"
 import { Separator } from "@/components/ui/separator"
 import { FormRenderer } from "@/components/form-renderer"
 import { TypographyH1, TypographyLead } from "@/components/typography"
+
+interface FormPageProperties {
+  params: {
+    id: string
+  }
+}
 
 const getForm = async ({ id }: { id: string }) => {
   const form = await db.query.forms.findFirst({
@@ -20,7 +29,48 @@ const getForm = async ({ id }: { id: string }) => {
   return form
 }
 
-const Form = async ({ params }: { params: { id: string } }) => {
+export async function generateMetadata({
+  params,
+}: FormPageProperties): Promise<Metadata> {
+  const form = await getForm({ id: params.id })
+
+  if (!form) {
+    return {}
+  }
+
+  const url = env.NEXT_PUBLIC_APP_URL
+
+  const ogUrl = new URL(`${url}/api/og`)
+  ogUrl.searchParams.set("heading", form.title)
+  ogUrl.searchParams.set("type", "Form")
+
+  return {
+    title: form.title,
+    description: form.description,
+    openGraph: {
+      title: form.title,
+      description: form.description || "",
+      type: "article",
+      url: absoluteUrl(`f/${form.id}`),
+      images: [
+        {
+          url: ogUrl.toString(),
+          width: 1200,
+          height: 630,
+          alt: form.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: form.title,
+      description: form.description || "",
+      images: [ogUrl.toString()],
+    },
+  }
+}
+
+const Form = async ({ params }: FormPageProperties) => {
   const { id } = params
 
   const form = await getForm({ id })
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import { env } from "@/env.mjs"
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function absoluteUrl(path: string) {
+  return `${env.NEXT_PUBLIC_APP_URL}${path}`
 }
