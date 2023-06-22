506c93813b5f43d68c5c595cc340069b22fd2283
matheins <mail@mheins.de>
2023-06-22 13:42:09 +0200

    feat: remove sidebar nav

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -37,13 +37,8 @@ export default async function DashboardLayout({
           />
         </div>
       </header>
-      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
-        <aside className="hidden w-[200px] flex-col md:flex">
-          <DashboardNav items={dashboardConfig.sidebarNav} />
-        </aside>
-        <main className="flex w-full flex-1 flex-col overflow-hidden">
-          {children}
-        </main>
+      <div className="container flex-1 gap-12">
+        <main className="flex w-full flex-col overflow-hidden">{children}</main>
       </div>
       <SiteFooter className="border-t" />
     </div>
